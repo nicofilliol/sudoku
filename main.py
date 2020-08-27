601558df144b29f90767d3838599f42d0bee2579
nicofilliol <nicolas.filliol@outlook.com>
2020-08-27 11:10:40 +0200

    moved sudoku images

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from gui import main_gui
 def main():
     #board = Board("sudokus/board1.txt")
     #board = Board("sudokus/board2.txt")
-    #board = Board("resources/sudoku_hard.jpg")
+    #board = Board("sudokus/sudoku_hard.jpg")
     board = Board("webcam")
 
     #main_gui(board)
